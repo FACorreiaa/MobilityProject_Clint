95f68bc67c570a2380ba73614b39e96aa8bc5b94
Knox316 <fernandocorreia316@gmail.com>
2020-02-02 08:58:31 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'materialize-css/dist/js/materialize.min.js';
+import './styles/tailwind.css';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
