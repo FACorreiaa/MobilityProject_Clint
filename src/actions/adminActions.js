0609e9d4afc14726775b69337c71e7ffd28f1b3a
Knox316 <fernandocorreia316@gmail.com>
2020-02-03 10:54:50 +0000

    .

--- src/actions/adminActions.js
+++ src/actions/adminActions.js
@@ -88,6 +88,7 @@ export const getRentalData = () => {
         if (error.response) {
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
+          console.log(error);
           console.log(error.response.data);
           console.log(error.response.status);
           console.log(error.response.headers);
